532de61090b47cd8d40609d09525e6a57f3b5288
Jawada90 <92611931+Jawada90@users.noreply.github.com>
2022-06-20 17:08:16 -0400

    Create CreditCard.java

---
+++ src/CreditCard.java
@@ -0,0 +1,17 @@
+/*
+=========================================================================================================================
+Object Oriented Programming			
+		
+Task: Java-Bank-System
+Submission: Jawad Awada
+=========================================================================================================================
+*/ 
+public interface CreditCard { // interface for credit card
+	
+	/**
+	 * @return the card number
+	 */
+	public String getCardNumber(); // returns the card number
+	
+	
+}
