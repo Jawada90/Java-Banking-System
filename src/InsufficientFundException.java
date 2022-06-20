db87617cf256c0852843d02eec90ac341b989d83
Jawada90 <92611931+Jawada90@users.noreply.github.com>
2022-06-20 17:09:33 -0400

    Create InsufficientFundException.java

---
+++ src/InsufficientFundException.java
@@ -0,0 +1,17 @@
+/*
+=========================================================================================================================
+Object Oriented Programming			
+		
+Task: Java-Bank-System
+Submission: Jawad Awada
+=========================================================================================================================
+*/ 
+
+
+
+public class InfsufficientFundException extends Exception { // Exception class
+
+	public InfsufficientFundException(String message) { // constructor
+		super(message); // call super class constructor
+	}
+}
