49703130abde9c18aba17d1875b1d59208695939
Jawada90 <92611931+Jawada90@users.noreply.github.com>
2022-06-20 17:03:57 -0400

    Create AccountNotFoundException.java

---
+++ src/AccountNotFoundException.java
@@ -0,0 +1,16 @@
+/*
+=========================================================================================================================
+Object Oriented Programming			
+		
+Task: Java-Bank-System
+Submission: Jawad Awada
+=========================================================================================================================
+*/ 
+
+
+public class AccountNotFoundException  extends Exception { // Exception class
+
+	public AccountNotFoundException(String message) { // constructor
+		super(message); // call super class constructor
+	}
+}
