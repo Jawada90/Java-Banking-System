8a42aea509d879fb3b0871cd95397d76e815a482
Jawada90 <92611931+Jawada90@users.noreply.github.com>
2022-06-20 17:12:17 -0400

    Create Policies.java

---
+++ src/Policies.java
@@ -0,0 +1,16 @@
+/*
+=========================================================================================================================
+Object Oriented Programming			
+		
+Task: Java-Bank-System
+Submission: Jawad Awada
+=========================================================================================================================
+*/ 
+public interface Policies {
+
+	public static final double chequingFee = 1.5; // fee for chequing account
+	
+
+	public static final double savingsFee = 0.5; // fee for savings account
+	
+}
